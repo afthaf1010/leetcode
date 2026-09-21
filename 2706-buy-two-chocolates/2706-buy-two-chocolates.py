0b724feab57320553580e3bd1a0675f3406e2eab
afthaf1010 <afthafahamed01@gmail.com>
2026-09-21 10:03:30 +0530

    2706-buy-two-chocolates Solved

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.py
@@ -0,0 +1,9 @@
+class Solution:
+    def buyChoco(self, prices: List[int], money: int) -> int:
+        a=min(prices)
+        prices.remove(a)
+        b=min(prices)
+        if a+b>money:
+            return money
+        else:
+            return money-(a+b)
