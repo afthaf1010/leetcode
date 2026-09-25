05fdebe3b56a42d034e165e4ccff39374a1dba8f
afthaf1010 <afthafahamed01@gmail.com>
2026-09-25 08:42:46 +0530

    3550-smallest-index-with-digit-sum-equal-to-index Solved

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.py
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.py
@@ -1,6 +1,5 @@
 class Solution:
     def smallestIndex(self, nums: List[int]) -> int:
-        x=-1
         for i in range(len(nums)):
             s=0
             while nums[i]>0:
@@ -8,6 +7,5 @@ class Solution:
                 s+=l
                 nums[i]//=10
             if s==i:
-                x=i
-                break
-        return x
+                return s
+        return -1
